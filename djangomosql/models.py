d2a75258f93badd9fef43688722ab7a4efa5b239
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-17 23:48:49 +0800

    Refactoring
    Using underscore-leading names for variables.
    The MoSQL select parameters are now specified with **kwargs instead to make the call more extendable.

--- djangomosql/models.py
+++ djangomosql/models.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
+from django.db.models import Model
 from django.db.models.manager import Manager
 from django.db.models.query import RawQuerySet
-from mosql.query import select
+from django.db.models.loading import get_model
+from mosql.query import select, join
 try:
     basestring
 except NameError:   # If basestring is not a thing, just alias it to str
@@ -16,24 +19,24 @@ class MoQuerySet(object):
     def __init__(self, model=None, using=None):
         self.model = model
         self._db = using
-        self.raw_query_set = None
-        self.where = {}
+        self._raw_query_set = None
+        self._where = {}
 
     def __iter__(self):
         """Iterate through the queryset using the backed RawQuerySet"""
-        if self.raw_query_set is None:
+        if self._raw_query_set is None:
             raw_query = self.get_raw_query()
-            self.raw_query_set = RawQuerySet(
+            self._raw_query_set = RawQuerySet(
                 raw_query=raw_query, model=self.model, using=self._db
             )
-        return self.raw_query_set.__iter__()
+        return self._raw_query_set.__iter__()
 
     def __getitem__(self, k):
         return list(self)[k]
 
     def _clone(self):
         clone = MoQuerySet(self.model, self._db)
-        clone.where = self.where
+        clone._where = self._where.copy()
         return clone
 
     def count(self):
@@ -41,12 +44,14 @@ class MoQuerySet(object):
 
     def get_raw_query(self):
         """Invoke mosql.query.select to generate the raw query"""
-        where = self.where_mapping or None
-        return select(self.model._meta.db_table, where=where)
+        kwargs = {}
+        if self._where:
+            kwargs['where'] = self._where
+        return select(self.model._meta.db_table, **kwargs)
 
     def where(self, mapping):
         clone = self._clone()
-        clone.where.update(mapping)
+        clone._where.update(mapping)
         return clone
 
 

bc20ebe7d91cfedd3534620e0d150b681addc5e3
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-18 00:55:37 +0800

    Some more refactoring
    Implement _get_rawqueryset instead of get_raw_query. This can be reused better.
    Removed some unused imports.
    All occurances of MoQuerySet initialization now use kwargs

--- djangomosql/models.py
+++ djangomosql/models.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import copy
-from django.db.models import Model
 from django.db.models.manager import Manager
 from django.db.models.query import RawQuerySet
-from django.db.models.loading import get_model
-from mosql.query import select, join
+from mosql.query import select
 try:
     basestring
 except NameError:   # If basestring is not a thing, just alias it to str
@@ -19,35 +17,38 @@ class MoQuerySet(object):
     def __init__(self, model=None, using=None):
         self.model = model
         self._db = using
-        self._raw_query_set = None
+        self._rawqueryset = None
         self._where = {}
 
     def __iter__(self):
         """Iterate through the queryset using the backed RawQuerySet"""
-        if self._raw_query_set is None:
-            raw_query = self.get_raw_query()
-            self._raw_query_set = RawQuerySet(
-                raw_query=raw_query, model=self.model, using=self._db
-            )
-        return self._raw_query_set.__iter__()
+        return self._get_rawqueryset().__iter__()
 
     def __getitem__(self, k):
         return list(self)[k]
 
+    def __getattr__(self, name):
+        return getattr(self._get_rawqueryset(), name)
+
     def _clone(self):
-        clone = MoQuerySet(self.model, self._db)
+        clone = MoQuerySet(model=self.model, using=self._db)
         clone._where = self._where.copy()
+        clone._joins = copy.copy(self._joins)
         return clone
+
+    def _get_rawqueryset(self):
+        if self._rawqueryset is None:
+            kwargs = {}
+            if self._where:
+                kwargs['where'] = self._where
+            raw_query = select(self.model._meta.db_table, **kwargs)
+            self._rawqueryset = RawQuerySet(
+                raw_query=raw_query, model=self.model, using=self._db
+            )
+        return self._rawqueryset
 
     def count(self):
         return len(list(self))
-
-    def get_raw_query(self):
-        """Invoke mosql.query.select to generate the raw query"""
-        kwargs = {}
-        if self._where:
-            kwargs['where'] = self._where
-        return select(self.model._meta.db_table, **kwargs)
 
     def where(self, mapping):
         clone = self._clone()

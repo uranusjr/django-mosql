e3077977644b87697201f8d47aee5d51e66e5bf2
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-21 17:50:14 +0800

    Drop Python 3 support for now

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],

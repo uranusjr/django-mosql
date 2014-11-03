d1c60ba989d784784c462be498285022ac78b211
Tzu-ping Chung <uranusjr@gmail.com>
2014-11-03 16:21:24 +0800

    Put requirement list into files

--- setup.py
+++ setup.py
@@ -5,21 +5,32 @@ import os
 from setuptools import setup
 import djangomosql
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 VERSION = djangomosql.__version__
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def get_install_requires():
+    filename = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    with open(filename) as f:
+        return f.readlines()
+
+
+def get_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+        return f.read()
+
 
 setup(
     name='django-mosql',
     version=VERSION,
     packages=['djangomosql', 'djangomosql.db'],
     include_package_data=True,
-    install_requires=['django>=1.4.2', 'mosql>=0.8.1,<0.9.1'],
+    install_requires=get_install_requires(),
     license='BSD License',
     description='Django model integration for MoSQL.',
-    long_description=README,
+    long_description=get_readme(),
     url='http://github.com/uranusjr/django-mosql',
     author='Tzu-ping Chung',
     author_email='uranusjr@gmail.com',
